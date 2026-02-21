1d3156475e591c4a082d936b40020fa77d805ba8
cnPthPiGon <samurchris8@gmail.com>
2026-02-21 14:45:47 +0100

    Update BestAntiKick-Max-Possible.lua

--- BestAntiKick-Max-Possible.lua
+++ BestAntiKick-Max-Possible.lua
@@ -9,15 +9,15 @@ setreadonly(mt, false)
 old.__namecall = mt.__namecall
 mt.__namecall = newcclosure(function(self, ...)
     local m = getnamecallmethod():lower()
-    if m:find("kick") or m:find("ban") or m:find("shutdown") or m:find("teleport") or m:find("punish") or m:find("crash") or m:find("leave") or m:find("disconnect") or m:find("expel") or m:find("removeplayer") or m:find("forceleave") or m:find("moderation") then
+    if m:find("kick") or m:find("ban") or m:find("shutdown") or m:find("teleport") or m:find("punish") or m:find("crash") or m:find("leave") or m:find("disconnect") or m:find("expel") or m:find("removeplayer") or m:find("forceleave") or m:find("moderation") or m:find("adminkick") or m:find("serverkick") then
         return
     end
-    if (m == "destroy" or m == "remove" or m == "removeasync" or m == "destroyallchildren" or m == "clearallchildren" or m == "breakjoints") and (self == lp or self == lp.Character or self == lp.PlayerGui or self == lp.PlayerScripts or self == lp.Backpack or self == lp.CharacterAppearance or self == lp.CharacterAppearanceLoaded or self.Parent == lp or self:FindFirstAncestorWhichIsA("Player") == lp) then
+    if (m == "destroy" or m == "remove" or m == "removeasync" or m == "destroyallchildren" or m == "clearallchildren" or m == "breakjoints" or m == "explode" or m == "kill") and (self == lp or self == lp.Character or self == lp.PlayerGui or self == lp.PlayerScripts or self == lp.Backpack or self == lp.CharacterAppearance or self == lp.CharacterAppearanceLoaded or self.Parent == lp or self:FindFirstAncestorWhichIsA("Player") == lp or self:FindFirstAncestor("Players") == lp) then
         return
     end
-    if (m == "fireserver" or m == "invokeserver") then
+    if (m == "fireserver" or m == "invokeserver" or m == "fireserverasync" or m == "invokeserverasync") then
         local n = tostring(self):lower()
-        if n:find("kick") or n:find("ban") or n:find("shutdown") or n:find("teleport") or n:find("punish") or n:find("crash") or n:find("leave") or n:find("disconnect") or n:find("expel") or n:find("removeplayer") or n:find("forceleave") or n:find("moderation") or n:find("exploit") or n:find("cheat") or n:find("admin") then
+        if n:find("kick") or n:find("ban") or n:find("shutdown") or n:find("teleport") or n:find("punish") or n:find("crash") or n:find("leave") or n:find("disconnect") or n:find("expel") or n:find("removeplayer") or n:find("forceleave") or n:find("moderation") or n:find("admin") or n:find("exploit") or n:find("cheat") or n:find("serverkick") or n:find("anticheat") or n:find("punishment") then
             return
         end
     end
@@ -27,10 +27,10 @@ end)
 old.__index = mt.__index
 mt.__index = newcclosure(function(self, k)
     local lk = k:lower()
-    if self == lp and (lk:find("kick") or lk:find("ban") or lk:find("shutdown") or lk:find("teleport") or lk:find("punish") or lk:find("crash") or lk:find("leave") or lk:find("disconnect") or lk:find("destroy") or lk:find("remove")) then
+    if self == lp and (lk:find("kick") or lk:find("ban") or lk:find("shutdown") or lk:find("teleport") or lk:find("punish") or lk:find("crash") or lk:find("leave") or lk:find("disconnect") or lk:find("destroy") or lk:find("remove") or lk:find("expel") or lk:find("moderation")) then
         return function() end
     end
-    if (self == lp.Character or self == lp or self.Parent == lp or self:FindFirstAncestorWhichIsA("Player") == lp) and (lk:find("breakjoints") or lk:find("destroy") or lk:find("remove")) then
+    if (self == lp.Character or self == lp or self.Parent == lp or self:FindFirstAncestorWhichIsA("Player") == lp) and (lk:find("breakjoints") or lk:find("destroy") or lk:find("remove") or lk:find("explode") or lk:find("kill")) then
         return function() end
     end
     return old.__index(self, k)
@@ -39,7 +39,7 @@ end)
 old.__newindex = mt.__newindex
 mt.__newindex = newcclosure(function(self, k, v)
     local lk = k:lower()
-    if self == lp and (lk:find("kick") or lk:find("ban") or lk:find("shutdown") or lk:find("teleport") or lk:find("punish") or lk:find("crash") or lk:find("leave") or lk:find("disconnect") or (lk == "parent" and v == nil)) then
+    if self == lp and (lk:find("kick") or lk:find("ban") or lk:find("shutdown") or lk:find("teleport") or lk:find("punish") or lk:find("crash") or lk:find("leave") or lk:find("disconnect") or lk:find("expel") or (lk == "parent" and v == nil)) then
         return
     end
     if (self == lp.Character or self == lp) and (lk == "parent" and v == nil) then
@@ -66,7 +66,7 @@ end)
 
 old.__call = mt.__call
 mt.__call = newcclosure(function(self, ...)
-    if self == lp.Kick or self == lp.Ban or self == lp.Destroy or self == lp.Shutdown or self == lp.Teleport or self == lp.Punish then
+    if self == lp.Kick or self == lp.Ban or self == lp.Destroy or self == lp.Shutdown or self == lp.Teleport or self == lp.Punish or self == lp.Expel then
         return
     end
     return old.__call and old.__call(self, ...) or self(...)
@@ -80,6 +80,7 @@ if hookfunction then
     if lp.Destroy then hookfunction(lp.Destroy, function() end) end
     if lp.Shutdown then hookfunction(lp.Shutdown, function() end) end
     if lp.Teleport then hookfunction(lp.Teleport, function() end) end
+    if lp.Punish then hookfunction(lp.Punish, function() end) end
 end
 
 lp.Kick = function() end
@@ -87,3 +88,4 @@ lp.Ban = function() end
 if lp.Destroy then lp.Destroy = function() end end
 if lp.Shutdown then lp.Shutdown = function() end end
 if lp.Teleport then lp.Teleport = function() end end
+if lp.Punish then lp.Punish = function() end end
